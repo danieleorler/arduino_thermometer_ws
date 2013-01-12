cfca56409949d54f7765239f0ceaf58b6a1b954c
Daniele Orler <daniele.orler@gmail.com>
2013-01-12 22:33:20 +0100

    entry point for survay fix

--- web.js
+++ web.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var User = require('./models/user.js');
-var User = require('./models/user.js');
+var Survey = require('./models/survey.js');
 var app = express();
 
 app.get('/', function(request, response)
 {
-    var user        = new User();
-    user.name       = "Test";
-    user.surname    = "User";
-    user.email      = "test@skodeg-o.com";
-    user.password   = "password";
-    user.save(function(err,user)
-    {
-        if(err)
-            console.log(err);
-        if(user)
-            console.log("User: "+ user.email + " saved!");
-    });
+    // var user        = new User();
+    // user.name       = "Test";
+    // user.surname    = "User";
+    // user.email      = "test@skodeg-o.com";
+    // user.password   = "password";
+    // user.save(function(err,user)
+    // {
+    //     if(err)
+    //         console.log(err);
+    //     if(user)
+    //         console.log("User: "+ user.email + " saved!");
+    // });
 
     response.send('Hello World!!');
 });
