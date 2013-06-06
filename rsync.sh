63b626bc771c98321b7642ae79874ab819175e61
Daniele Orler <daniele.orler@gmail.com>
2013-06-06 21:47:34 +0200

    remove rsync

--- rsync.sh
+++
@@ -1,3 +0,0 @@
-#rsync --exclude .svn -avz -e ssh -i esb.pem ../docroot/ root@ec2-50-19-68-217.compute-1.amazonaws.com./var/lib/mule/apps/bpm_chat/docroot
-
-rsync --exclude-from 'rsync_exclude' -avz -e "ssh -i /Users/dalen/.ssh/developer" /Users/dalen/Sites/arduinothermometer/* dalen@192.168.56.101:/home/dalen/projects/arduinothermometer
